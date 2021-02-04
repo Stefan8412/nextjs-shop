78d04d00fdfde0f53dc467c90e4c2125e35cb00d
Stefan Hancar <stefan@hancar.sk>
2021-02-04 20:51:01 +0100

    add static stuff

---
+++ pages/ssg.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const url = "https://pokeapi.co/api/v2/pokemon?limit=151";
+const headers = { "Cache-Control": "no-cache" };
+
+const StaticSide = (props) => {
+  return props.pokemon.map((poke) => {
+    return (
+      <div key={poke.name}>
+        <img src={poke.imgUrl} />
+        <p>{poke.name}</p>
+        <hr />
+      </div>
+    );
+  });
+};
+
+export const getStaticProps = async () => {
+  const response = await axios.get(url, { headers });
+  const promises = response.data.results.map((result) => {
+    return axios.get(result.url, { headers });
+  });
+  const responses = await Promise.all(promises);
+  // console.log(responses);
+
+  const pokeData = responses.map((r) => {
+    return {
+      name: r.data.name,
+      imgUrl: r.data.sprites.front_default,
+    };
+  });
+  return {
+    props: {
+      pokemon: pokeData,
+    },
+  };
+};
+
+export default StaticSide;
